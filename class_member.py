36dee69c7d824459c6bd6f5ceb853da7f30a264b
SJ <yws7236@gmail.com>
2024-02-23 21:03:26 +0900

    2st push

--- class_member.py
+++ class_member.py
@@ -32,21 +32,3 @@ for member in members:
     print(member.name)
 
 posts = []
-for member in members:
-    for i in range(3):  # 각 회원당 3개의 게시글 작성
-        title = f"{member.name}의 게시글 {"개시글", i+1}"
-        content = f"{member.name}의 게시글 내용 {"내용", i+1}"
-        post = Post(title, content, member.username)
-        posts.append(post)
-
-# 특정 유저가 작성한 게시글 제목 출력
-print("특정 유저가 작성한 게시글 제목:")
-for post in posts:
-    if post.author == "Kim":
-        print(post.title)
-
-# 특정 단어가 포함된 게시글 제목 출력
-print("\n특정 단어가 포함된 게시글 제목:")
-for post in posts:
-    if '1' in post.content:
-        print(post.title)
